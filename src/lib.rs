4fbc0a5c9e4fd21fc46f47e4804323347d901a88
Vadim Chekan <kot.begemot@gmail.com>
2018-01-04 00:18:44 -0500

    Start sketching API

--- src/lib.rs
+++ src/lib.rs
@@ -14,55 +14,73 @@ mod parquet;
 use std::fs::{OpenOptions};
 use std::io::prelude::*;
 use std::io::{BufReader, SeekFrom};
-
 use thrift::protocol::{TCompactInputProtocol};
-
 use parquet::*;
+
 
 const MAGIC: &'static str ="PAR1";
 
-fn read(file_name: String) -> std::io::Result<()> {
-    let mut foptions = OpenOptions::new();
-    let mut unbuffered = foptions.read(true).open(file_name)?;
-    let mut buffered = BufReader::new(unbuffered);
-
-
-    // read footer metadta length and magic
-    buffered.seek(SeekFrom::End(-(4 + 4)))?;
-    let mut buf8: [u8; 4] = [0; 4];
-    buffered.read(buf8.as_mut())?;
-    let footer_len: u32 = (buf8[0] as u32) | (buf8[1] as u32) << 8 | (buf8[2] as u32) << 16 | (buf8[3] as u32) << 24;
-    println!("footer len: {}, {:?}", footer_len, buf8);
-
-    // magic
-    buffered.read_exact(buf8.as_mut())?;
-    if MAGIC.as_bytes().ne(&buf8) {
-        return Err(std::io::Error::new(std::io::ErrorKind::Other, "Bad format"));
-    }
-
-    // TODO: check footer size for sanity
-    let pos = buffered.seek(SeekFrom::Current(-(footer_len as i64 +8_i64))).expect("File metadata position failed");
-
-    let mut protocol = TCompactInputProtocol::new(buffered);
-    let fileMeta = FileMetaData::read_from_in_protocol(& mut protocol).expect("Failed to deserialize file metadata");
-    println!("Version {}, rows: {}, row_groups: {}\n    created_by {:?}\n    key_value_metadata: {:?}",
-        fileMeta.version,
-        fileMeta.num_rows,
-        fileMeta.row_groups.len(),
-        fileMeta.created_by,
-        fileMeta.key_value_metadata
-    );
-
-    //fileMeta.row_groups.get(0).expect("No row group found")
-
-    return Ok(());
+pub struct Reader {
+    pub info: Info,
+    file_meta: FileMetaData,
 }
+pub struct Info {
+    pub version: i32,
+    pub num_rows: i64,
+    pub row_groups: usize,
+    pub created_by: String
+}
+
+impl Reader {
+    pub fn open(file_name: String) -> std::io::Result<Reader> {
+        let mut foptions = OpenOptions::new();
+        let mut unbuffered = foptions.read(true).open(file_name)?;
+        let mut buffered = BufReader::new(unbuffered);
+
+        // read footer metadata length and magic
+        buffered.seek(SeekFrom::End(-(4 + 4)))?;
+        let mut buf4: [u8; 4] = [0; 4];
+        buffered.read_exact(buf4.as_mut())?;
+        let footer_len: u32 = (buf4[0] as u32) | (buf4[1] as u32) << 8 | (buf4[2] as u32) << 16 | (buf4[3] as u32) << 24;
+        println!("footer len: {}, {:?}", footer_len, buf4);
+
+        // magic
+        buffered.read_exact(buf4.as_mut())?;
+        if MAGIC.as_bytes().ne(&buf4) {
+            return Err(std::io::Error::new(std::io::ErrorKind::Other, "Bad format"));
+        }
+
+        // TODO: check footer size for sanity
+        let pos = buffered.seek(SeekFrom::Current(-(footer_len as i64 +8_i64))).expect("File metadata position failed");
+
+        let mut protocol = TCompactInputProtocol::new(buffered);
+        let fileMeta = FileMetaData::read_from_in_protocol(& mut protocol).expect("Failed to deserialize file metadata");
+
+        return Ok(Reader {
+            info: Info {
+                version: fileMeta.version,
+                num_rows: fileMeta.num_rows,
+                row_groups: fileMeta.row_groups.len(),
+                created_by: fileMeta.created_by.clone().unwrap_or(String::from(""))
+            },
+            file_meta: fileMeta
+        });
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn it_works() {
-        read("test-data/test1.snappy.parquet".to_string()).expect("Failed to read paruet file");
+        let reader = Reader::open("test-data/test1.snappy.parquet".to_string()).expect("Failed to read parquet file");
+        println!("Version {}, rows: {}, row_groups: {}\n    created_by {:?}",
+                 reader.info.version,
+                 reader.info.num_rows,
+                 reader.info.row_groups,
+                 reader.info.created_by
+        );
+
     }
 }
